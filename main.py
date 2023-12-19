60df8847ec68009b568e4deb01790d7a3bd4325a
codergirl1106 <68753302+codergirl1106@users.noreply.github.com>
2023-12-19 14:47:47 -0500

    main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ import requests
 import streamlit_scrollable_textbox as stx
 import streamlit_js_eval
 
-st.write(f"Screen width is _{streamlit_js_eval.streamlit_js_eval(js_expressions='screen.width', want_output = True, key = 'SCR')}_")
+swidth = streamlit_js_eval.streamlit_js_eval(js_expressions='screen.width', want_output = True, key = 'SCR')
 
 root = './adapter-free-Model'
 
